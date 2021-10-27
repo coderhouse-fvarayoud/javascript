f32e335e25e5a3b874d2e6afff4da33aca8a6636
Fede Varayoud <varayoudfederico@gmail.com>
2021-10-27 10:43:20 -0300

    Fix for empty input

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ console.log("Inicio");
 let suma = 0;
 let input = 0;
 do {
-  input = prompt("Ingrese un numero (o un caracter para terminar): ");
-  if (isNaN(input)) alert(`La suma final es: ${suma}`);
+  input = prompt("Ingrese un número (ESC o un caracter para finalizar): ");
+  if (!input || isNaN(input)) alert(`La suma final es: ${suma}`);
   else {
     suma += parseInt(input);
     alert(`La suma actual es: ${suma}`);
   }
-} while (!isNaN(input));
+} while (input && !isNaN(input));
